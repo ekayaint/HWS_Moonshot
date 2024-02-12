900a06820485bd7068b344e63ef02696a50710f0
ekayaint <ekayaint@gmail.com>
2024-02-12 20:19:20 +0300

    Initial Commit

---
+++ HWS_Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HWS_Moonshot
+//
+//  Created by ekayaint on 12.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HWS_Moonshot/HWS_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_MoonshotApp.swift
+//  HWS_Moonshot
+//
+//  Created by ekayaint on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HWS_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
